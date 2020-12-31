03394a79edd28a8985a7a63e0d8c872fc3af17f1
lluis31 <76479676+lluis31@users.noreply.github.com>
2020-12-31 12:15:57 +0100

    ex1-udo9

---
+++ ex1-udo9/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ex1_udo9
+{
+    class Alumno
+    {
+        private string nombre;
+        private int edad;
+
+        public Alumno()
+        {
+            Console.Write("Ingrese nombre:");
+            nombre = Console.ReadLine();
+            Console.Write("Ingrese edad:");
+            string linea = Console.ReadLine();
+            edad = int.Parse(linea);
+        }
+
+        public void Imprimir()
+        {
+            Console.WriteLine("Nombre:" + nombre);
+            Console.WriteLine("Edad:" + edad);
+        }
+
+        public void EsMayorEdad()
+        {
+            if (edad >= 18)
+            {
+                Console.Write(nombre + " es mayor de edad.");
+            }
+            else
+            {
+                Console.Write(nombre + " no es mayor de edad.");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Alumno alumno1 = new Alumno();
+            alumno1.Imprimir();
+            alumno1.EsMayorEdad();
+            Console.ReadKey();
+        }
+    }
+}
