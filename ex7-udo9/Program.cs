ea329f2c1697aea76e4f80248ae870bc6474d0ec
lluis31 <76479676+lluis31@users.noreply.github.com>
2021-01-04 15:13:25 +0100

    ex8-udo9

---
+++ ex7-udo9/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ex8_udo9
+{
+    class Persona2
+    {
+        public string nombre;
+        public int edad;
+        public string DNI;
+        public string sexo;
+        public int peso;
+        public int altura;
+        public const string s = "H";
+
+        public Persona2()
+        {
+            this.nombre = "";
+            this.edad = 0;
+            this.DNI = "12345678A";
+            this.sexo = s;
+            this.peso = 0;
+            this.altura = 0;
+        }
+        public Persona2(string nombre, int edad, string sexo)
+        {
+            this.nombre = nombre;
+            this.edad = edad;
+            this.DNI = "12345678A";
+            this.sexo = sexo;
+            this.peso = 0;
+            this.altura = 0;
+        }
+        public Persona2(string nombre, int edad, string DNI, string sexo, int peso, int altura)
+        {
+            this.nombre = nombre;
+            this.edad = edad;
+            this.DNI = DNI;
+            this.sexo = sexo;
+            this.peso = peso;
+            this.altura = altura;
+        }
+
+
+
+
+
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
