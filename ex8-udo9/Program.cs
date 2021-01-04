66c546a02f59442905d7084dc12708e2826f5b8c
lluis31 <76479676+lluis31@users.noreply.github.com>
2021-01-04 15:21:08 +0100

    ex8-udo9

---
+++ ex8-udo9/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ex8_udo9
+{
+    class Password
+    {
+        public int Longitud { get; set; }
+        public int Contraseña { get; set; }
+        public void Crear()
+        {
+            this.Longitud = 8;
+            Random r = new Random();
+            this.Contraseña = r.Next(10000000, 99999999);
+        }
+
+
+
+        static void Main(string[] args)
+        {
+       
+        }
+    }
+}
