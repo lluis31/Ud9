cb057a1855a791cf7b9c3f0eac3ff9a3998f06ce
lluis31 <76479676+lluis31@users.noreply.github.com>
2021-01-04 15:03:32 +0100

    ex5-udo9

---
+++ ex5-udo9/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ex5_udo9
+{
+    class Libro
+    {
+        public string autor { get; set; }
+        public string titulo { get; set; }
+        public string ubicacion { get; set; }
+
+        public void Nuevo(string a, string t, string u)
+        {
+            this.autor = a;
+            this.titulo = t;
+            this.ubicacion = u;
+        }
+        public void Mostrar()
+        {
+            Console.WriteLine("Titulo: {0}. Autor: {1}. Ubicación:{2}.", this.titulo, this.autor, this.ubicacion);
+        }
+        public static void Ex5_udo9()
+        {
+            Libro L1 = new Libro();
+            L1.Nuevo("Lluis Planas", "La vida con CSharp", "Valls");
+            L1.Mostrar();
+        }
+    }
+}
