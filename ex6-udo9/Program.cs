e9c8f8aa2fdade31c4eb80b2968e0bf9f1b4792d
lluis31 <76479676+lluis31@users.noreply.github.com>
2021-01-04 15:08:37 +0100

    ex6-udo9

---
+++ ex6-udo9/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ex6_udo9
+{
+    class Program
+    {
+        public string marca { get; set; }
+        public string modelo { get; set; }
+        public int cilindrada { get; set; }
+        public double potencia { get; set; }
+
+        public void Nuevo(string marca, string modelo, int cilindrada, double potencia)
+        {
+            this.marca = marca;
+            this.modelo = modelo;
+            this.cilindrada = cilindrada;
+            this.potencia = potencia;
+        }
+        public void Mostrar()
+        {
+            Console.WriteLine("Marca: {0}. Modelo: {1}. Cilindrada:{2}. Potencia:{3}", this.marca, this.modelo, this.cilindrada, this.potencia);
+        }
+
+        public static void Ejercicio6()
+        {
+            Coche C1 = new Coche();
+            C1.Nuevo("Ford", "mustang", 460, 5.0);
+            C1.Mostrar();
+        }
+
+
+
+
+
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
